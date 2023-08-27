2eba04079998aa8404ade4b8c98c49a151246b64
tom <nqwrt@naver.com>
2023-08-27 11:37:06 +0900

    읽기까지 완성

---
+++ src/models/Board.js
@@ -0,0 +1,13 @@
+class Board {
+  constructor(bid, bname, bcontent, btitle, bdate, bhit, bstep, bgroup, bindent) {
+    this.bid = bid
+    this.bcontent = bcontent
+    this.bname = bname
+    this.btitle = btitle
+    this.bdate = bdate
+    this.bhit = bhit
+    this.bstep = bstep
+    this.bgroup = bgroup
+    this.bindent = bindent
+  }
+}
